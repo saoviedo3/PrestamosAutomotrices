b1dd778534cd51b256a5753487fb8488d19c6143
nsvillegas@espe.edu.ec <nsvillegas@espe.edu.ec>
2025-05-25 16:55:03 -0500

    manejo de respository-documentos

--- originacion/src/main/java/com/banquito/sistema/originacion/repository/DocumentoAdjuntoRepository.java
+++ originacion/src/main/java/com/banquito/sistema/originacion/repository/DocumentoAdjuntoRepository.java
@@ -1,5 +1,23 @@
 package com.banquito.sistema.originacion.repository;
 
-public class DocumentoAdjuntoRepository {
+import com.banquito.sistema.originacion.model.DocumentoAdjunto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+public interface DocumentoAdjuntoRepository extends JpaRepository<DocumentoAdjunto, Integer> {
+
+    List<DocumentoAdjunto> findBySolicitudCredito_IdSolicitud(Integer idSolicitud);
+
+    List<DocumentoAdjunto> findByTipoDocumento_IdTipoDocumento(Integer idTipoDocumento);
+
+    List<DocumentoAdjunto> findBySolicitudCredito_IdSolicitudAndTipoDocumento_IdTipoDocumento(Integer idSolicitud,
+            Integer idTipoDocumento);
+
+    boolean existsBySolicitudCredito_IdSolicitudAndTipoDocumento_IdTipoDocumento(Integer idSolicitud,
+            Integer idTipoDocumento);
+
+    long countBySolicitudCredito_IdSolicitud(Integer idSolicitud);
 }
